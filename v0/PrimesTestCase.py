8b283247251db25520b3fd4ed9e7b6c6d428d9f5
Igor Steinmacher <igorsteinmacher@gmail.com>
2022-03-31 17:24:54 -0300

    Update PrimesTestCase.py

--- v0/PrimesTestCase.py
+++ v0/PrimesTestCase.py
@@ -3,7 +3,7 @@ from prime import is_prime
 
 class PrimesTestCase(unittest.TestCase):
    def test_is_five_prime(self):    
-      self.assertTrue(is_prime(5)) 
+      self.assertFalse(is_prime(5)) 
    def test_is_nine_prime(self):
       self.assertFalse(is_prime(9))
    def test_is_negative_prime(self):
